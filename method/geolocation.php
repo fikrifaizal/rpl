ee07770a2908259f981ba227e7076e6d20f25dbf
fikrifaizal <fikrifaizal40@gmail.com>
2021-12-21 22:36:57 +0700

    add geo-location

--- method/geolocation.php
+++ method/geolocation.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>RPL</title>
-
-    <!-- panggil css -->
-    <link rel="stylesheet" href="/assets/css/main.css">
-
-    <!-- panggil file manifest -->
-    <link rel="manifest" href="/manifest.json">
-
-    <!-- panggil app (service worker) -->
-    <script src="/assets/js/app.js"></script>
-
-    <link rel="shortcut icon" href="/assets/images/logo-96x96.png" content="#FFE1C4">
-    <meta name="theme-color" content="#FFE1C4">
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <title>Geo-Location</title>
 
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <!-- Javascript Support -->
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </head>  
+  </head>
 
   <body>
-    <div class="content">
-      <div class="text-center">
-        <h1>RPL</h1>
-        <h1>PWA with Dynamic Cache</h1><br>
-      </div>
+    <p>Click the button to get your coordinates.</p>
 
-      <a role="button" class="btn btn-primary" href="/method/geolocation.php">Geo-Location</a>
-    </div>
+    <button onclick="getLocation()">Try It</button>
+
+    <p id="demo"></p>
+
+    <script>
+      var x = document.getElementById("demo");
+
+      function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+      }
+
+      function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+          "<br>Longitude: " + position.coords.longitude;
+      }
+    </script>
   </body>
 </html>
